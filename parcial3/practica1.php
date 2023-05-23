cf518a05cfaaca853f3e5d115a4973cd135cec42
Eitan <124410416+InzunzaE@users.noreply.github.com>
2023-05-22 18:59:44 -0600

    Practica 1 PHP 22/05/23

---
+++ parcial3/practica1.php
@@ -0,0 +1,20 @@
+<?php
+    
+    echo "<h1>hola mundo</h1>";
+
+    echo "<hr>";
+    
+    $nombre = "Eitan";
+    $edad = 18;
+    
+    if($edad >= 18) {
+    echo $nombre . "es mayor de dad";
+    } else {
+        echo $nombre . "es  menor de edad";
+    }
+
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+
+?>
